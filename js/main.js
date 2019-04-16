8617033fdf847c5399166fe1b3f5a5a484b66e49
Tech-A24 <tech-a24@Tech-A24s-MacBook-Air.local>
2019-04-16 13:49:53 +0100

    first question loop and page finished

--- js/main.js
+++ js/main.js
@@ -18,24 +18,109 @@ console.log("hello world");
     // next screen gives instructions with button to start game
 
 // BUTTON INPUTS
-var buttonInput = $("button");
-var correctAnswer = true;
-var buttonPressed = "";
-// var correctAnswer = api.index[] -> find the correct answer within api array
+var $buttons = $(".button");
+var $buttonPressed = "";
 
-$("button").on("click", function () {
-  if (buttonPressed == "") {
+var maxNum = 4;
+var randomAssigner =  Math.floor((Math.random() * (maxNum)));
+
+var $question = "";
+var $correctAnswer = "";
+var $answers = [];
+// creat a var for question - apply value to question
+// create a new array and pull the three incorrect
+// creat new var for correct for each button random math to ap
+
+function assignData() {
+
+$.get("https://opentdb.com/api.php?amount=1&difficulty=easy&type=multiple", function(data) {
+    $question = data.results[0].question;
+    $("#question-box").html($question);
+    $correctAnswer = data.results[0].correct_answer;
+    assignCorrectAnswer();
+    $answers = data.results[0].incorrect_answers;
+    assignAnswers();
+  });
+}
+
+
+function assignCorrectAnswer() {
+  if (randomAssigner == 0) {
+    $("#0").html("A. " + $correctAnswer);
+    $correctAnswer = "A. " + $correctAnswer;
+  }
+  else if (randomAssigner == 1) {
+    $("#1").html("B. " + $correctAnswer);
+    $correctAnswer = "B. " + $correctAnswer;
+  }
+  else if (randomAssigner == 2) {
+    $("#2").html("C. " + $correctAnswer);
+    $correctAnswer = "C. " + $correctAnswer;
+  }
+  else if (randomAssigner == 3) {
+    $("#3").html("D. " + $correctAnswer);
+    $correctAnswer = "D. " + $correctAnswer;
+  }
+  console.log($correctAnswer);
+}
+
+function assignAnswers() {
+  for (var i = 0; i < $buttons.length; i++) {
+    if ($("#0").html() == "") {
+      $("#0").html("A. " + $answers[i]);
+      $answers[i]--;
+    }
+    else if ($("#1").html() == "") {
+      $("#1").html("B. " + $answers[i]);
+      $("#1").html("B. " + $answers[i]);
+      $answers[i]--;
+    }
+    else if ($("#2").html() == "") {
+      $("#2").html("C. " + $answers[i]);
+      $("#2").html("C. " + $answers[i]);
+      $("#2").html("C. " + $answers[i]);
+      $answers[i]--;
+    }
+    else if ($("#3").html() == "") {
+      $("#3").html("D. " + $answers[i]);
+      $("#3").html("D. " + $answers[i]);
+      $("#3").html("D. " + $answers[i]);
+      $answers[i]--;
+    }
+  }
+}
+
+function selectAndOutput() {
+
+$($buttons).on("click", function () {
+  if ($buttonPressed == "") {
     $(this).addClass("selected-answer");
-    buttonPressed = "answered";
+    $buttonPressed = $(this);
     console.log(this.value + " was clicked");
+    console.log($buttonPressed);
   }
   setTimeout(function() {
-    $("#B").addClass("correct-answer").html("Correct answer");
+    if ($correctAnswer == $buttonPressed.html()) {
+      $buttonPressed.addClass("correct-answer").html("Correct Answer!");
+    }
+    else {
+      for (var i = 0; i < $buttons.length; i++) {
+        if ($($buttons[i]).html() == $correctAnswer) {
+          $($buttons[i]).addClass("correct-answer").html("Correct Answer!");
+        }
+      }
+    }
   }, 2000);
-  if (correctAnswer = true) {
+  if ($correctAnswer == $buttonPressed.html()) {
     setTimeout(function() {
-      $("#B").css("background-color", "gray").html("Click again for your next question!");
-      $(".speech-bubble").html("Congratulations! You've won £2,000,000!");
+      $buttonPressed.css("background-color", "gray").html("Click again for your next question!");
+      $(".speech-bubble").html('"Congratulations! You\'ve won £!""').addClass(" message").css("background-color", "green");
+    }, 4000); // THESE DON'T CURRENTLY WORK
+  }
+  else if ($correctAnswer != $buttonPressed.html()) {
+    setTimeout(function() {
+      $buttonPressed.css("background-color", "gray").html("God, I'm sorry but you really are garbage! TRY AGAIN?");
+      $(".speech-bubble").html('"Awwww, poor you. You leave today with nothing!"').addClass(" message").css("background-color", "red");
     }, 4000);
   }
 
@@ -47,8 +132,10 @@ $("button").on("click", function () {
   //   console.log("start next questions");
   // })
 })
+}
 
-
+assignData();
+selectAndOutput();
 // As a new player
 // I need to see the question clearly
 // and see my options clearly
